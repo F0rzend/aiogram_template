a5332dd1caad76bd7766849c2dfd5b3bc7682377
forzend <kosyajog@mail.ru>
2021-02-16 21:47:17 +0300

    parse mode html by default

--- bot/main.py
+++ bot/main.py
@@ -6,7 +6,7 @@ from .handlers import StartHandler
 
 async def main(config: dict):
     logging.basicConfig(level=logging.DEBUG)
-    bot = Bot(token=config['bot']['token'])
+    bot = Bot(token=config['bot']['token'], parse_mode='HTML')
     dp = Dispatcher(use_builtin_filters=True)
     dp.message.register(StartHandler, commands=['start'])
     await dp.start_polling(bot)
