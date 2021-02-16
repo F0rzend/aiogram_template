486569a63e5ba768755f1e1cf25ad53f256cee4e
forzend <kosyajog@mail.ru>
2021-02-16 21:43:54 +0300

    base logging

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,11 @@
 from aiogram import Bot, Dispatcher
+import logging
 
 from .handlers import StartHandler
 
 
 async def main(config: dict):
+    logging.basicConfig(level=logging.DEBUG)
     bot = Bot(token=config['bot']['token'])
     dp = Dispatcher(use_builtin_filters=True)
     dp.message.register(StartHandler, commands=['start'])
