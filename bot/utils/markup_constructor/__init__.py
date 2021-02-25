721c3ed9ea1362f8c27c54997607a403c9f78d35
F0rzend <kostyajog@mail.ru>
2021-02-25 21:02:29 +0300

    Add Markup Constructors

---
+++ bot/utils/markup_constructor/__init__.py
@@ -0,0 +1,11 @@
+from .inline import InlineMarkupConstructor
+from .reply import ReplyMarkupConstructor
+from aiogram.types import InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButtonPollType
+
+__all__ = (
+    "InlineMarkupConstructor",
+    "ReplyMarkupConstructor",
+    "InlineKeyboardMarkup",
+    "ReplyKeyboardMarkup",
+    "KeyboardButtonPollType"
+)
--- bot/utils/startup_notify.py
+++ bot/utils/startup_notify.py
@@ -3,15 +3,12 @@ from typing import List, Union
 
 from .broadcast import Broadcast
 
-from aiogram.types import ReplyKeyboardRemove
-
 
 async def notify_superusers(chats: Union[List[int], List[str], int, str]):
     count = await (
         Broadcast(
             users=chats,
             text='<b>The bot is running!</b>',
-            reply_markup=ReplyKeyboardRemove(),
         )
     ).run()
     logging.info(f"{count} users received start messages")
