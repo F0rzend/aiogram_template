f45e55758ea754fabd8af72498bd854df2f81c40
F0rzend <kostyajog@mail.ru>
2021-02-27 17:08:42 +0300

    Update Broadcaster.
    All arguments in __init__ method.
    Template text for Broadcast

--- bot/utils/broadcast.py
+++ bot/utils/broadcast.py
@@ -1,42 +1,47 @@
 import asyncio
 import logging
-import typing
+from typing import Union, Optional, List, Dict, NoReturn
 from asyncio import sleep
+from string import Template
 
 from aiogram import Bot, types
 from aiogram.utils import exceptions
 
-chat_id_type = typing.Union[typing.List[int], typing.List[str], int, str]
+ChatIdType = Union[int, str]
+ChatIdsType = Union[List[ChatIdType], ChatIdType]
+ChatsType = Union[ChatIdsType, List[Dict]]
 
 
 class Broadcast:
     def __init__(
         self,
-        users: chat_id_type,
-        text: str,
-        parse_mode: typing.Optional[str] = None,
-        reply_markup: typing.Union[
+        chats: ChatsType,
+        text: Union[Template, str],
+        parse_mode: Optional[str] = None,
+        disable_web_page_preview: Optional[bool] = None,
+        disable_notification: Optional[bool] = None,
+        reply_to_message_id: Optional[int] = None,
+        allow_sending_without_reply: Optional[bool] = None,
+        reply_markup: Union[
             types.InlineKeyboardMarkup,
             types.ReplyKeyboardMarkup,
             types.ReplyKeyboardRemove,
             types.ForceReply,
             None,
         ] = None,
-        bot: typing.Optional[Bot] = None,
+        bot: Optional[Bot] = None,
         timeout: float = 0.02,
         logger=__name__,
     ):
-        if isinstance(users, list):
-            self.users = users
-        elif isinstance(users, int) or isinstance(users, str):
-            self.users = [users]
-        else:
-            raise AttributeError(f"Expected type '{chat_id_type}', got '{type(users)}' instead")
-        self.text = text
+        self._setup_chats(chats)
+        self.text = Template(text) if isinstance(text, str) else text
         self.parse_mode = parse_mode
+        self.disable_web_page_preview = disable_web_page_preview
+        self.disable_notification = disable_notification
+        self.reply_to_message_id = reply_to_message_id
+        self.allow_sending_without_reply = allow_sending_without_reply
         self.reply_markup = reply_markup
         self.bot = bot if bot else Bot.get_current()
-        self.count = 0
         self.timeout = timeout
 
         if not isinstance(logger, logging.Logger):
@@ -44,61 +49,77 @@ class Broadcast:
 
         self.logger = logger
 
+    def _setup_chats(self, chats: ChatsType):
+        if isinstance(chats, int) or isinstance(chats, str):
+            self.chats = [{'chat_id': chats}]
+        elif isinstance(chats, list):
+            if all([
+                isinstance(chat, int) or isinstance(chat, str)
+                for chat in chats
+            ]):
+                self.chats = [
+                    {'chat_id': chat} for chat in chats
+                ]
+            elif all([
+                isinstance(chat, dict)
+                for chat in chats
+            ]):
+                if not all([chat.get('chat_id') for chat in chats]):
+                    raise ValueError('Not all dictionaries "chat_id" key')
+                if not (len(set([tuple(chat.keys()) for chat in chats])) == 1):
+                    raise ValueError('Not all dictionaries have identical keys')
+                self.chats = [
+                    {'chat_id': args.pop('chat_id'), **args} for args in chats if args.get('chat_id', None)
+                ]
+        else:
+            raise TypeError(f'pwd: expected {Union[ChatIdsType, List[Dict]]}, got "type(chats)"')
+
     async def send_message(
             self,
-            user_id: typing.Union[int, str],
-            disable_web_page_preview: typing.Optional[bool] = None,
-            disable_notification: typing.Optional[bool] = None,
-            reply_to_message_id: typing.Optional[int] = None,
-            allow_sending_without_reply: typing.Optional[bool] = None,
-
+            chat: Dict,
     ) -> bool:
+        if isinstance(chat, Dict):
+            chat_id = chat.get('chat_id')
+            text_args = chat
+        else:
+            return False
         try:
             await self.bot.send_message(
-                chat_id=user_id,
-                text=self.text,
+                chat_id=chat_id,
+                text=self.text.safe_substitute(text_args),
                 parse_mode=self.parse_mode,
-                disable_web_page_preview=disable_web_page_preview,
-                disable_notification=disable_notification,
-                reply_to_message_id=reply_to_message_id,
-                allow_sending_without_reply=allow_sending_without_reply,
+                disable_web_page_preview=self.disable_web_page_preview,
+                disable_notification=self.disable_notification,
+                reply_to_message_id=self.reply_to_message_id,
+                allow_sending_without_reply=self.allow_sending_without_reply,
                 reply_markup=self.reply_markup,
             )
         except exceptions.RetryAfter as e:
             self.logger.debug(
-                f"Target [ID:{user_id}]: Flood limit is exceeded. Sleep {e.timeout} seconds."
+                f"Target [ID:{chat_id}]: Flood limit is exceeded. Sleep {e.timeout} seconds."
             )
             await sleep(e.timeout)
-            return await self.send_message(user_id)  # Recursive call
+            return await self.send_message(chat_id)  # Recursive call
         except (
                 exceptions.BotBlocked,
                 exceptions.ChatNotFound,
                 exceptions.UserDeactivated,
                 exceptions.ChatNotFound
         ) as e:
-            self.logger.debug(f"Target [ID:{user_id}]: {e.match}")
+            self.logger.debug(f"Target [ID:{chat_id}]: {e.match}")
         except exceptions.TelegramAPIError:
-            self.logger.exception(f"Target [ID:{user_id}]: failed")
+            self.logger.exception(f"Target [ID:{chat_id}]: failed")
         else:
-            self.logger.debug(f"Target [ID:{user_id}]: success")
+            self.logger.debug(f"Target [ID:{chat_id}]: success")
             return True
         return False
 
-    async def run(
-            self,
-            disable_web_page_preview: typing.Optional[bool] = None,
-            disable_notification: typing.Optional[bool] = None,
-            reply_to_message_id: typing.Optional[int] = None,
-            allow_sending_without_reply: typing.Optional[bool] = None,
-    ) -> int:
-        for user_id in self.users:
+    async def run(self) -> NoReturn:
+        count = 0
+        for chat in self.chats:
             if await self.send_message(
-                    user_id,
-                    disable_web_page_preview,
-                    disable_notification,
-                    reply_to_message_id,
-                    allow_sending_without_reply,
+                    chat=chat
             ):
-                self.count += 1
+                count += 1
             await asyncio.sleep(self.timeout)
-        return self.count
+        logging.info(f'{count}/{len(self.chats)} messages sent out')
--- bot/utils/startup_notify.py
+++ bot/utils/startup_notify.py
@@ -1,14 +1,13 @@
-import logging
 from typing import List, Union
+
+from aiogram import md
 
 from .broadcast import Broadcast
 
 
 async def notify_superusers(chats: Union[List[int], List[str], int, str]):
-    count = await (
-        Broadcast(
-            users=chats,
-            text='<b>The bot is running!</b>',
-        )
-    ).run()
-    logging.info(f"{count} users received start messages")
+    chats = [
+        {'chat_id': chat_id, 'mention': md.hlink(title=f'ID:{chat_id}', url=f'tg://user?id={chat_id}')}
+        for chat_id in chats
+    ]
+    await Broadcast(chats=chats, text=md.hbold('$mention, The bot is running!'), parse_mode='HTML').run()
